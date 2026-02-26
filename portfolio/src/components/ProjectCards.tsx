0ca1a84f89b00483c363c685f4895f319364e0bb
Elise Frazier <efrazie7@uncc.edu>
2026-02-25 19:56:50 -0500

    fix project cards alignment

--- portfolio/src/components/ProjectCards.tsx
+++ portfolio/src/components/ProjectCards.tsx
@@ -9,48 +9,46 @@ import {
   Typography,
 } from "@mui/material";
 
-
 const StyledButton = styled(Button)({
   backgroundColor: "pink",
-  '&:hover':{
-    backgroundColor:'#E75480'
-  }
+  "&:hover": {
+    backgroundColor: "#E75480",
+  },
 });
-interface Url{
-  id:number;
-  name:string;
-  url:string;
-  previewImg:string;
-  description:string;
+interface Url {
+  id: number;
+  name: string;
+  url: string;
+  previewImg: string;
+  description: string;
 }
-interface ProjectProps{
-  urls:Url[];
+interface ProjectProps {
+  urls: Url[];
 }
-const ProjectCards:React.FC<ProjectProps> =({urls}) =>  {
+const ProjectCards: React.FC<ProjectProps> = ({ urls }) => {
   return (
-    <Grid container spacing={7}>
-      {urls.map(item => (
+    <Grid container spacing={7} sx={{justifyContent:'center'}}>
+      {urls.map((item) => (
         <Grid key={item.id} size="auto">
-
-           <Card key={item.id} sx={{maxWidth:345}}>
-          <CardMedia
-            component="img"
-            height="194"
-            image={item.previewImg}
-            alt="previewImage"
-          />
-          <CardContent>
-            <Typography variant="h5">{item.name}</Typography>
-            <Typography variant="body1">{item.description}</Typography>
-            <StyledButton variant="contained" href={item.url}>
-              View Project
-            </StyledButton>
-          </CardContent>
-        </Card>
+          <Card key={item.id} sx={{ maxWidth: 345 }}>
+            <CardMedia
+              component="img"
+              height="194"
+              image={item.previewImg}
+              alt="previewImage"
+            />
+            <CardContent>
+              <Typography variant="h5">{item.name}</Typography>
+              <Typography variant="body1">{item.description}</Typography>
+              <StyledButton variant="contained" href={item.url}>
+                View Project
+              </StyledButton>
+            </CardContent>
+          </Card>
         </Grid>
       ))}
     </Grid>
   );
-}
+};
 
 export default ProjectCards;
